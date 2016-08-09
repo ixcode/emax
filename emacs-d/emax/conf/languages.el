facaf405bd4bce3ee881366edd9353ab1a855a9a
Jim Barritt <jim@planet-ix.com>
2016-08-09 11:35:15 +0100

    Fixing ansi colors in shell and adding node repl

--- emacs-d/emax/conf/languages.el
+++ emacs-d/emax/conf/languages.el
@@ -370,3 +370,9 @@
 
 (global-flycheck-mode)
 
+(require 'nodejs-repl)
+
+(add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
+(add-to-list 'comint-output-filter-functions 'ansi-color-process-output)
+
+
