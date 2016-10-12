c7db8762b3419cb640d7d1293b8386abf0807f2d
Jim Barritt <jimb@thoughtworks.com>
2016-10-12 11:32:02 +0100

    Updating Journal Entry
    - Allow it to create in current dir

--- emacs-d/emax/conf/journal.el
+++ emacs-d/emax/conf/journal.el
@@ -1,11 +1,27 @@
-;; Provides functions that create journal entries for a diary like journal.
+;;; journal.el --- Provides functions that create journal entries for a diary like journal
 
-(defun journal-entry-filename ()  
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(defun journal-entry-filename ()
+  "The format of a journal entry."
   (format-time-string "JOURNAL-ENTRY-%Y-%m-%m-%H:%M.md"))
 
-(defun journal-entry ()
+(defun journal-entry-home ()
+  "Create a journal entry in your home dir."
   (interactive)
-  (find-file (concat "/Users/jmdb/Journal/" (journal-entry-filename))))
+  (find-file (concat "~/Journal/" (journal-entry-filename))))
 
+(defun journal-entry ()
+  "Create a journal entry in the current dir."
+  (interactive)
+  (find-file (journal-entry-filename)))
+
+(provide 'journal)
+
+;;; journal.el ends here
 
 
